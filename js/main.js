08f7db4059c8d0069dbd26a85b291f16d2eb4424
jdu214843 <214843a@jdu.uz>
2023-02-11 15:35:19 +0500

    update javascript

--- js/main.js
+++ js/main.js
@@ -1,93 +1,91 @@
-import products from "./products.json" assert { type: "json" };
-
-const main = document.querySelector("main");
-
-for (let i = 0; i < products.length; i++) {
-    const product = document.createElement("div");
-    product.classList.add('door');
-    product.innerHTML =
-    "<img src='images/" +
-        products[i].image +
-    "' >" +
-    "<p>"+
-        products[i].name +
-    "</p>"+
-    "<p>" +
-        products[i].price + "$" +
-   "</p>"+
-   "<button class='btn-add'>1" +
-        products[i].button +
-   "</button>"
-
-   main.append(product);
-};
-
-
-
-const aside = document.querySelector("aside");
-
-for (let i = 0; i < products.length; i++) {
-    const product = document.createElement("div");
-    product.classList.add('card');
-    product.innerHTML =
-    "<img src='images/" +
-    products[i].image +
-    "' >" +
-    "<div class='item-card1'>"+
-        "<p class='title'>"+
-            products[i].name +
-        "</p>"+
-         "<p>" +
-            products[i].price + "$" +
-        "</p>"+
-    "</div>"+
-    "<div class='item-card2'>"+
-    "<button class='btn-add'>" +
-            products[i].buttonadd +
-        "</button>"+
-        "<input class='display'>"+
-        "<button class='btn-add'>" +
-            products[i].buttonminus +
-        "</button>"+
-    "</div>"+
-    "<div class='item-card3'>"+
-        "<i class='fa fa-trash'>"+
-        "</i>"+
-    "</div>"
+import products from "./products.json" assert { type: "json" }; 
+ 
+const main = document.querySelector("main"); 
+ 
+ 
+for (let i = 0; i < products.length; i++) { 
+  const product = document.createElement("div"); 
+  product.classList.add('product') 
+  product.innerHTML = 
+  ` 
+  <img src='images/${products[i].image}'>  
+  <p> 
+    ${products[i].name} 
+  </p> 
    
-   aside.append(product);
-};
+  <p> 
+    $${products[i].price}  
+  </p>  
+ 
+  <button class='btn-add' data-id='${products[i].id}'> 
+  ${products[i].button} 
+  </button> 
+` 
+ 
+ 
+  main.append(product); 
+}; 
+
+
+
+ 
+const aside = document.querySelector("aside");  
+ 
+document.querySelectorAll('.btn-add').forEach(btn => { 
+  btn.addEventListener('click', buy) 
+}) 
+ 
+function buy(event) { 
+  let id = event.target.dataset.id; 
+      for (let i = 0; i < products.length; i++) { 
+        if (id == products[i].id) { 
+          let element = products[i]; 
+          const product = document.createElement("div"); 
+          product.classList.add('card') 
+          product.innerHTML = 
+                "<div class='description'>"+
+                    "<img src='images/" + 
+                    element.image +  
+                    "' >" + 
+                    "<p class='name'>"+ 
+                    element.name + 
+                    "</p>"+ 
+                    "<p>"+ 
+                    element.price + "$" + 
+                    "</p>" +
+                "</div>"+
+                "<div class='btn-dec_inc'>"+
+                    "<button>" +
+                        element.buttondec + 
+                    "</button>"+
+                    "<input type='number' disabled>"+
+                    "<button>" +
+                        element.buttoninc + 
+                    "</button>"+
+                "</div>"+
+                "<div class='remove-trash'>"+
+                    "<i class='fa fa-trash remove'></i>"+
+                "</div>"
+           
+         
+          aside.append(product); 
+
+          let remove = document.querySelectorAll(".remove");
+            remove = remove[remove.length - 1]
+            remove.addEventListener("click", (e) => {
+                e.target.parentNode.parentNode.remove()
+            })
+          break; 
+        } 
+         
+      } 
+}
 
 // add to card onclick
 
 
-const RemoveCarditem = document.getElementsByClassName("fa-trash");
-    for(let i = 0; i < RemoveCarditem.length; i++){
-        let button = RemoveCarditem[i]
-        button.addEventListener("click", function(click){
-            let btnCliked = click.target
-            btnCliked.parentElement.parentElement.remove()
-            // totalUpdate()
-        });
-        
-    };
 
-// const none = document.querySelector("aside");
-//     none.style.display = "none";
-//     const AddCard = document.getElementById("btn-adds");
-//     AddCard.addEventListener("click", function(){
-//         none.style.display = "block"
-//         setInterval(() => {
-//             none.style.display = "none"
-//         }, 5000);
-//     })
 
-const addToCard = document.querySelectorAll(".btn-add");
-    addToCard.forEach(btn => {
-        btn.addEventListener("click", (e) => {
-            
-        })
-    })
 
 
 
